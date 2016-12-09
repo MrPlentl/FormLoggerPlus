184998d40493325d30ed9c660eb5e269a99a3a3d
Brandon Plentl <bplentl@gmail.com>
2016-12-09 15:18:03 -0600

    Fixed an issue that wasn't logging button clicks

--- tests/demo.php
+++ tests/demo.php
@@ -40,7 +40,6 @@ $log->Trace(1);
 ?>
 <html>
 <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="../lib/ajax_log2file.js"></script>
 </head>
