bdd669207e1c85123e1e714f49810e2af4a4ed06
Brandon Plentl <bplentl@gmail.com>
2017-07-31 10:22:37 -0500

    Added a couple comments about loading the JS and jQuery

--- tests/demo.php
+++ tests/demo.php
@@ -34,8 +34,10 @@ $log->Trace(1);
 ?>
 <html>
 <head>
+    <!-- jQuery is Required for the AJAX calls -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
+    <!-- ajax_log2file.js must also be included to execute the client side logging -->
     <script src="/js/form-logger-plus/ajax_log2file.js"></script>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
